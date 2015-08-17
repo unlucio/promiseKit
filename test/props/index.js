986bf5b47c963c9c822229d1b25e8e178a980ee5
Michele Bertoli <michele@berto.li>
2015-08-17 10:00:15 +0100

    test

---
+++ test/props/index.js
@@ -0,0 +1,69 @@
+var assert = require('assert');
+
+require('../../props');
+
+describe('props', function() {
+
+  it('should be a function', function() {
+
+    assert.equal(typeof Promise.props, 'function');
+
+  });
+
+  it('should return a res object with the given keys', function(done) {
+
+    var data1 = {data: '1'};
+    var promise1 = function() {
+      return new Promise(function(resolve) {
+        resolve(data1);
+      });
+    };
+
+    var data2 = {data: '2'};
+    var promise2 = function() {
+      return new Promise(function(resolve) {
+        resolve(data2);
+      });
+    };
+
+    var promises = {
+      data1: promise1(),
+      data2: promise2()
+    };
+
+    Promise.props(promises).then(function(res) {
+      assert.equal(res.data1, data1);
+      assert.equal(res.data2, data2);
+    }).then(done, done);
+
+  });
+
+  it('should return a res object with the given keys (no matter the order)', function(done) {
+
+    var data1 = {data: '1'};
+    var promise1 = function() {
+      return new Promise(function(resolve) {
+        setTimeout(resolve.bind(null, data1), 1000);
+      });
+    };
+
+    var data2 = {data: '2'};
+    var promise2 = function() {
+      return new Promise(function(resolve) {
+        setTimeout(resolve.bind(null, data2), 500);
+      });
+    };
+
+    var promises = {
+      data1: promise1(),
+      data2: promise2()
+    };
+
+    Promise.props(promises).then(function(res) {
+      assert.equal(res.data1, data1);
+      assert.equal(res.data2, data2);
+    }).then(done, done);
+
+  });
+
+});
