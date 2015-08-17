25381d81efb723384c5450e730fad2ce0bcdc477
Emanuele DelBono <emanuele@codiceplastico.com>
2015-08-17 23:01:53 +0200

    Rewritten with a "more functional" approach.

--- props/index.js
+++ props/index.js
@@ -1,17 +1,15 @@
 Promise.props = function(map) {
   var requests = [];
   var keys = Object.keys(map);
-
-  keys.forEach(function(key) {
-    requests.push(map[key]);
-  });
+  
+  requests = keys.map(function(key){ return map[key];})
 
   return Promise.all(requests).then(function(results) {
-    var resultsMap = {};
-
-    results.forEach(function(value, index) {
-      resultsMap[keys[index]] = value;
-    });
+    
+    var resultsMap = results.reduce(function(map, res, index){
+      map[keys[index]] = res;
+      return map;
+    }, {});
 
     return resultsMap;
   });
