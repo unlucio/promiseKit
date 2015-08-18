cc6c5461397955f310f08aa5bac73d9d90a22bb3
Emanuele DelBono <emanuele@codiceplastico.com>
2015-08-18 08:46:45 +0200

    Joined declaration with assignment of requests and returned directly from reduce

--- props/index.js
+++ props/index.js
@@ -1,16 +1,11 @@
 Promise.props = function(map) {
-  var requests = [];
   var keys = Object.keys(map);
-  
-  requests = keys.map(function(key){ return map[key];})
+  var requests = keys.map(function(key){ return map[key];})
 
   return Promise.all(requests).then(function(results) {
-    
-    var resultsMap = results.reduce(function(map, res, index){
+    return results.reduce(function(map, res, index){
       map[keys[index]] = res;
       return map;
     }, {});
-
-    return resultsMap;
   });
 };
